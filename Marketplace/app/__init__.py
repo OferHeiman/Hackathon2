9f75ce7197422dec2a9a360aadb43f25a76372a3
OferHeiman <oferheiman@gmail.com>
2021-11-07 20:36:42 +0200

    update

--- Marketplace/app/__init__.py
+++ Marketplace/app/__init__.py
@@ -11,8 +11,8 @@ app.config['DEBUG'] = True
 os.system('export FLASK_APP=run.py')
 
 # Database Connection
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fgkhfcrgfprvxp:72440aaa640e835a4b93ed360f96255bc012cff4636fd3aabcd2a5d9dd447b09@ec2-54-80-137-25.compute-1.amazonaws.com:5432/d2ld2j9mf29dv4"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/marketplace"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fgkhfcrgfprvxp:72440aaa640e835a4b93ed360f96255bc012cff4636fd3aabcd2a5d9dd447b09@ec2-54-80-137-25.compute-1.amazonaws.com:5432/d2ld2j9mf29dv4"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost/marketplace"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Database Representation
